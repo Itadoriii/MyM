b5cdec45233aac6e0f920526f9b4a103c74aaafd
flintiis <120700934+flintiis@users.noreply.github.com>
2024-07-04 23:16:59 -0400

    Create bd.sql

---
+++ bd.sql
@@ -0,0 +1,150 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mym
+-- ------------------------------------------------------
+-- Server version	8.4.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id_cliente` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `celular` varchar(45) DEFAULT NULL,
+  `direccion` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'Sebastian','Castro','scastroc@utem.cl','971948026','calle3 2298','1312');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detalle_pedido`
+--
+
+DROP TABLE IF EXISTS `detalle_pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detalle_pedido` (
+  `id_detalle_pedido` int NOT NULL AUTO_INCREMENT COMMENT 'id del detalle',
+  `id_pedido` int DEFAULT NULL COMMENT 'id del pedido al cual corresponde',
+  `id_producto` int DEFAULT NULL COMMENT 'id del producto al cual corresponde',
+  `cantidad` int DEFAULT NULL COMMENT 'cantidad del producto',
+  `precio_detalle` int DEFAULT NULL COMMENT 'precio de la cantidad por el precio del producto',
+  `estado` varchar(45) DEFAULT NULL COMMENT 'estado del pedido: recibido,procesado,pagado,finalizado (definir cadena del pedido)',
+  PRIMARY KEY (`id_detalle_pedido`),
+  KEY `id_pedido_idx` (`id_pedido`),
+  KEY `id_produducto_idx` (`id_producto`),
+  CONSTRAINT `id_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id_pedido`),
+  CONSTRAINT `id_produducto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalle_pedido`
+--
+
+LOCK TABLES `detalle_pedido` WRITE;
+/*!40000 ALTER TABLE `detalle_pedido` DISABLE KEYS */;
+INSERT INTO `detalle_pedido` VALUES (1,NULL,1,3,15000,NULL);
+/*!40000 ALTER TABLE `detalle_pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedidos`
+--
+
+DROP TABLE IF EXISTS `pedidos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedidos` (
+  `id_pedido` int NOT NULL AUTO_INCREMENT COMMENT 'identificador del pedido',
+  `id_cliente` int DEFAULT NULL COMMENT 'identificador del cliente que realizo el pedido',
+  `precio_total` int DEFAULT NULL COMMENT 'precio total del pedido',
+  `fecha_pedido` varchar(45) DEFAULT NULL COMMENT 'fecha en que se realizo el pedido\n',
+  `id_detalle_pedido` int DEFAULT NULL COMMENT 'id de todos los detalles de pedido',
+  PRIMARY KEY (`id_pedido`),
+  KEY `id_detalle_pedido_idx` (`id_detalle_pedido`),
+  KEY `id_cliente_idx` (`id_cliente`),
+  CONSTRAINT `id_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`),
+  CONSTRAINT `id_detalle_pedido` FOREIGN KEY (`id_detalle_pedido`) REFERENCES `detalle_pedido` (`id_detalle_pedido`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedidos`
+--
+
+LOCK TABLES `pedidos` WRITE;
+/*!40000 ALTER TABLE `pedidos` DISABLE KEYS */;
+INSERT INTO `pedidos` VALUES (1,1,45000,'4/7/2024',1);
+/*!40000 ALTER TABLE `pedidos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `id_producto` int NOT NULL AUTO_INCREMENT COMMENT 'identificador\n',
+  `nombre_prod` varchar(45) DEFAULT NULL COMMENT 'Nombre del producto',
+  `precio_unidad` int DEFAULT NULL COMMENT 'precio en clp',
+  `medidas` varchar(45) DEFAULT NULL COMMENT 'medida en metros',
+  `tipo` varchar(45) DEFAULT NULL COMMENT 'cepillada o bruta',
+  `fecha_add` varchar(45) DEFAULT NULL COMMENT 'fecha en la que se añadio el producto',
+  `disponibilidad` varchar(45) DEFAULT NULL COMMENT 'enStock o sinStock',
+  `dimensiones` varchar(45) DEFAULT NULL COMMENT 'medida del volumen ej 3x3x2 5x5x2 etc',
+  PRIMARY KEY (`id_producto`),
+  UNIQUE KEY `id_producto_UNIQUE` (`id_producto`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (1,'Pino oregon',15000,'3m','cepillado','7/4/2024','30','5x5x2'),(2,'Pino oregon',6666,'1m','natural','7/4/2024','2','5x3x1'),(3,'Madera normal',12,'5m','cepillado','7/4/2024','2','2x2x2'),(4,'Tablas',242,'2m','natural','7/4/2024','2','2x3x4');
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-04 22:38:51
