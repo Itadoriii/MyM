ba034282442d35c53a56487fdeb2932f11149842
Itadoriii <86499855+Itadoriii@users.noreply.github.com>
2024-07-08 22:40:43 -0400

    no recuerdoq  hice

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import session from 'express-session';
 import { methods as metodos } from './controllers/authentication.controller.js';
 import { methods as authorization } from './middlewares/authorization.js';
+import './middlewares/passport-setup.js'; // Importa la configuración de passport
 import pool from './db.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -19,6 +22,9 @@ app.listen(port, () => {
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'src')));
 app.use(cookieParser());
+app.use(session({ secret: 'your_secret_key', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // RUTAS 
 app.get('/', authorization.soloPublico, (req, res) => {
@@ -26,22 +32,29 @@ app.get('/', authorization.soloPublico, (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(__dirname  + '/src/login.html');
+  res.sendFile(__dirname + '/src/login.html');
 });
 
 app.get('/register', authorization.soloPublico, (req, res) => {
-  res.sendFile(__dirname  + '/src/register.html');
+  res.sendFile(__dirname + '/src/register.html');
 });
 
 app.post('/api/register', metodos.register);
 app.post('/api/login', metodos.login);
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/admin');
+  });
+
 app.get('/admin', authorization.soloAdmin, (req, res) => {
   res.sendFile(__dirname + '/src/admin.html');
 });
 
-app.get('/admin', (req, res) => {
-  res.sendFile(__dirname + '/src/admin.html');
-});
 app.get('/profile', (req, res) => {
   res.sendFile(__dirname + '/src/profile.html');
 });
