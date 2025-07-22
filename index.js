b22c90b05c38e520cfbc37e531bdfcc1ec631de6
sebas <scastroc@utem.cl>
2025-07-22 19:25:23 -0400

    server port

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // SERVIDOR 
 const app = express();
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = process.env.PORT;
+app.listen(port, '127.0.0.1', () => {
   console.log(`Servidor corriendo en puerto ${port}`);
 });
 
