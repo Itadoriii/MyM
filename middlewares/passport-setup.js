f8dd8dfd9185cbc25e81516f84dff71aad4466b9
sebas <scastroc@utem.cl>
2025-07-22 22:03:15 -0400

    mod google

--- middlewares/passport-setup.js
+++ middlewares/passport-setup.js
@@ -9,7 +9,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL_PROD
+    callbackURL: "https://sebastiancastro.cl/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
