22525dcea8937293820aa73d4616b577c2aa584c
sebas <scastroc@utem.cl>
2025-08-02 16:38:59 -0400

    coreccion de sesion en profile

--- src/profile.js
+++ src/profile.js
@@ -52,3 +52,15 @@ document.addEventListener('DOMContentLoaded', async () => {
     console.error('Error al cargar datos del usuario:', error);
   }
 });
+
+document.querySelector('.button').addEventListener('click', async () => {
+    const res = await fetch('/logout', {
+      method: 'GET'
+    });
+    if (res.redirected) {
+      window.location.href = res.url; // Redirige al usuario a la página de inicio de sesión
+    }
+  });
+
+
+
