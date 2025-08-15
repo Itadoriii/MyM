e1fb22275d523f338b0812dc49c0d9b7c440c84a
sebas <scastroc@utem.cl>
2025-08-14 22:11:21 -0400

    cambios login.js 2

--- src/login.js
+++ src/login.js
@@ -5,7 +5,7 @@ document.getElementById("login-form").addEventListener("submit",async (e)=>{
   e.preventDefault();
   const user = e.target.children.user.value;
   const password = e.target.children.password.value;
-  const res = await fetch("http://sebastiancastro.cl/api/login",{
+  const res = await fetch("https://sebastiancastro.cl/api/login",{
     method:"POST",
     headers:{
       "Content-Type":"application/json"
