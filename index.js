bd7577f4794c4ae68194d4ced15c9dea78db1a25
sebas <scastroc@utem.cl>
2025-08-15 01:05:18 -0400

    intento de correccion 33

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.post('/api/login', metodos.login);
 
 // Ejemplo Trabajadores
 
-app.get('/api/trabajadores', async (req, res) => {
+app.get('/trabajadores', async (req, res) => {
   try {
     const [rows] = await pool.query("SELECT * FROM trabajadores ORDER BY id_trabajador DESC");
     res.json({
