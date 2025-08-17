0170712bb4d1da2a1f8071e2be0171f0a904a116
sebas <scastroc@utem.cl>
2025-08-16 23:10:42 -0400

    change cookie

--- controllers/authentication.controller.js
+++ controllers/authentication.controller.js
@@ -33,7 +33,7 @@ async function login(req, res) {
         res.cookie("jwt", token, {
             httpOnly: true,
             secure: false,  // Para desarrollo local, cambiar a true si estás en producción con HTTPS
-            sameSite: 'None', // Asegura que la cookie sea enviada con peticiones "same-site"
+            sameSite: 'Lax', // Asegura que la cookie sea enviada con peticiones "same-site"
             maxAge: process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000,
             domain: ".maderasmym.cl"
         });
